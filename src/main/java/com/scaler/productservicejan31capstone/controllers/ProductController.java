b8f5f8964a1229c24ad111e378bc8a56b361b236
Rajneesh Kumar <RajneeshPondy@users.noreply.github.com>
2025-02-18 12:23:13 +0530

    Exception handlling

--- src/main/java/com/scaler/productservicejan31capstone/controllers/ProductController.java
+++ src/main/java/com/scaler/productservicejan31capstone/controllers/ProductController.java
@@ -1,26 +1,65 @@
 package com.scaler.productservicejan31capstone.controllers;
 
+import com.scaler.productservicejan31capstone.dtos.CreateFakeStoreProductDto;
 import com.scaler.productservicejan31capstone.dtos.ProductResponseDto;
+import com.scaler.productservicejan31capstone.exceptions.ProductNotFoundException;
 import com.scaler.productservicejan31capstone.models.Product;
-import com.scaler.productservicejan31capstone.services.FakeStoreProductService;
+import com.scaler.productservicejan31capstone.services.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
-    FakeStoreProductService fakeStoreProductService;
+    ProductService productService;
 
-    public ProductController(FakeStoreProductService fakeStoreProductService) {
-        this.fakeStoreProductService = fakeStoreProductService;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
     @GetMapping("/products/{id}")
-    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable long id){
-        Product product = fakeStoreProductService.getProductById(id);
+    public ResponseEntity<ProductResponseDto> getProductById(@PathVariable long id) throws ProductNotFoundException {
+        Product product = productService.getProductById(id);
         ProductResponseDto productResponseDto = ProductResponseDto.from(product);
         return new ResponseEntity<>(productResponseDto, HttpStatus.OK);
     }
+
+    @GetMapping("/products")
+    public List<ProductResponseDto> getAllProducts() throws ProductNotFoundException {
+       List<Product> products = productService.getAllProducts();
+
+       List<ProductResponseDto> productResponseDtos = new ArrayList<>();
+       for (Product product : products) {
+           ProductResponseDto productResponseDto = ProductResponseDto.from(product);
+           productResponseDtos.add(productResponseDto);
+       }
+//       List<ProductResponseDto> productResponseDtos =
+//               products.stream().map(ProductResponseDto::from).toList();
+       return productResponseDtos;
+    }
+
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(@RequestBody CreateFakeStoreProductDto createFakeStoreProductDto){
+        Product product = productService.createProduct(
+                createFakeStoreProductDto.getName(),
+                createFakeStoreProductDto.getPrice(),
+                createFakeStoreProductDto.getDescription(),
+                createFakeStoreProductDto.getImageUrl(),
+                createFakeStoreProductDto.getCategory());
+
+        ProductResponseDto productResponseDto = ProductResponseDto.from(product);
+        return productResponseDto;
+    }
+
+//    @ExceptionHandler(NullPointerException.class)
+//    public ErrorDto handleNullPointerException(){
+//        ErrorDto errorDto = new ErrorDto();
+//        errorDto.setStatus("Failure");
+//        errorDto.setMessage("Product not found");
+//        return errorDto;
+//    }
 }
--- src/main/java/com/scaler/productservicejan31capstone/services/FakeStoreProductService.java
+++ src/main/java/com/scaler/productservicejan31capstone/services/FakeStoreProductService.java
@@ -1,9 +1,14 @@
 package com.scaler.productservicejan31capstone.services;
 
 import com.scaler.productservicejan31capstone.dtos.FakeStoreProductDto;
+import com.scaler.productservicejan31capstone.dtos.FakeStoreProductRequestDto;
+import com.scaler.productservicejan31capstone.exceptions.ProductNotFoundException;
 import com.scaler.productservicejan31capstone.models.Product;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class FakeStoreProductService implements ProductService {
@@ -14,12 +19,48 @@ public class FakeStoreProductService implements ProductService {
         this.restTemplate = restTemplate;
     }
 
-    public Product getProductById(long id) {
+    public Product getProductById(long id) throws ProductNotFoundException {
         FakeStoreProductDto fakeStoreProductDto = restTemplate.getForObject(
                 "https://fakestoreapi.com/products/" + id,
                     FakeStoreProductDto.class);
+        if(fakeStoreProductDto == null)
+        {
+            throw new ProductNotFoundException("The product for id " + id + " does not exist");
+        }
+
+        return fakeStoreProductDto.toProduct();
+    }
+    
+    public List<Product> getAllProducts() throws ProductNotFoundException {
+        FakeStoreProductDto[] fakeStoreProductDtos = restTemplate.getForObject("https://fakestoreapi.com/products",
+                FakeStoreProductDto[].class);
+        List<Product> products= new ArrayList<>();
+
+        if(fakeStoreProductDtos == null)
+        {
+            throw new ProductNotFoundException("Product list is empty");
+        }
+
+        for (FakeStoreProductDto fakeStoreProductDto : fakeStoreProductDtos) {
+            Product product= fakeStoreProductDto.toProduct();
+            products.add(product);
+        }
+        return products;
+    }
+
+    public Product createProduct(String name, double price, String description, String imageUrl, String category) {
+        FakeStoreProductRequestDto fakeStoreProductRequestDto = new FakeStoreProductRequestDto();
+        fakeStoreProductRequestDto.setTitle(name);
+        fakeStoreProductRequestDto.setPrice(price);
+        fakeStoreProductRequestDto.setDescription(description);
+        fakeStoreProductRequestDto.setImage(imageUrl);
+        fakeStoreProductRequestDto.setCategory(category);
+
+        FakeStoreProductDto fakeStoreProductDto = restTemplate.postForObject(
+                "https://fakestoreapi.com/products",
+                fakeStoreProductRequestDto ,
+                FakeStoreProductDto.class);
         assert fakeStoreProductDto != null;
         return fakeStoreProductDto.toProduct();
     }
-
 }
